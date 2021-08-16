35000ad50cf286ea813e4b25382e53b631c607c1
Cody S <cody.s.snow@gmail.com>
2021-08-16 19:31:03 -0400

    added image folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,37 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = ({ cityDetails, weather }) => {
+
+    // const cityDetails = data.cityDetails;
+    // const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Imperial.Value}</span>
+        <span>&deg;F</span>
+        </div>
+    `;
+
+    // remove d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
 
 const updateCity = async (city) => {
-    // getCity(city).then(data => {
-    //     return getWeather(data.Key);
-    // }).then(data => {
-    //     console.log(data);
-    // }).catch(err => console.log(err));
 
     const cityDetails = await getCity(city);
     const weather = await getWeather(cityDetails.Key);
+
+    return { cityDetails, weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -20,6 +43,8 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // update UI with new city
-    updateCity(city);
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
 });
 
